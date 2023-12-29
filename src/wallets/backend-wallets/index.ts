1fd257ce408eebd9bef76f67c83c48497734dc59
WebWizrd8 <superultradev000@gmail.com>
2023-12-28 20:07:02 -0500

    Apply suggestions from code review

--- src/wallets/backend-wallets/index.ts
+++ src/wallets/backend-wallets/index.ts
@@ -4,8 +4,9 @@ export function getActiveBackendWalletByChainId(
   chainId: number = EVMChainId.ETH,
 ) {
   switch (chainId) {
-    default:
     case EVMChainId.ETH:
+      return "0x9Aa13F15BdC45995b86e43129681693077f39777";
+    default:
       return "0x9Aa13F15BdC45995b86e43129681693077f39777";
   }
 }
